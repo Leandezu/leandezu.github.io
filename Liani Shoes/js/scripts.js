7b78311340eaddf01c07f26f7b7f5a9165f43714
Leandezu <81389031+Leandezu@users.noreply.github.com>
2023-03-26 21:01:50 -0300

    1.1

--- Liani Shoes/js/scripts.js
+++ Liani Shoes/js/scripts.js
@@ -1,7 +1,13 @@
-const mensajeOfertas = document.querySelector('.header__ofertas');
 const botonSwitch = document.querySelector('#switch');
+const body = document.querySelector('body');
+const navegador = document.querySelector('.navegador');
+const logo = document.querySelector('h1');
+const enlaces = document.querySelectorAll('.enlaces');
+const header = document.querySelector('.header');
+const svg = document.querySelectorAll('.redes__svg');
+const mensajeOfertas = document.querySelector('.header__ofertas');
 
-console.log(mensajeOfertas);
+console.log(logo);
 
 setInterval(() =>{
     if(mensajeOfertas.textContent == 'Ofertas todos los fin de semana!'){
@@ -10,3 +16,15 @@ setInterval(() =>{
         mensajeOfertas.textContent = 'Ofertas todos los fin de semana!'
     }
 }, 3000)
+
+botonSwitch.addEventListener('click', () => {
+    body.classList.toggle('modoOscuro');
+    navegador.classList.toggle('modoOscuro');
+    logo.classList.toggle('color-blanco');
+    enlaces[0].classList.toggle('color-blanco');
+    enlaces[0].classList.toggle('color-negro');
+    enlaces[1].classList.toggle('color-blanco');
+    enlaces[1].classList.toggle('color-negro');
+});
+
+console.log('body');
