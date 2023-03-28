0a7c539348f664dcd6cd7fe8a25e8ad044adc655
Leandezu <81389031+Leandezu@users.noreply.github.com>
2023-03-27 23:13:52 -0300

    1.3

--- Liani Shoes/js/scripts.js
+++ Liani Shoes/js/scripts.js
@@ -7,6 +7,7 @@ const header = document.querySelector('.header');
 const svg = document.querySelectorAll('.redes__svg');
 const mensajeOfertas = document.querySelector('.header__ofertas');
 const btnCatalogo = document.querySelector('#btnCatalogo');
+const footer = document.querySelector('footer');
 
 setInterval(() =>{
     if(mensajeOfertas.textContent == 'Ofertas todos los fin de semana!'){
@@ -17,12 +18,18 @@ setInterval(() =>{
 }, 3000)
 
 botonSwitch.addEventListener('click', () => {
-    body.classList.toggle('modoOscuro');
+    body.classList.toggle('fondo-grisOscuro');
+    body.classList.toggle('color-blanco');
+    header.classList.toggle('fondo-negro');
+    header.classList.toggle('fondo-grisOscuro');
     navegador.classList.toggle('modoOscuro');
     logo.classList.toggle('color-blanco');
     enlaces[0].classList.toggle('color-blanco');
     enlaces[0].classList.toggle('color-negro');
     enlaces[1].classList.toggle('color-blanco');
     enlaces[1].classList.toggle('color-negro');
-    btnCatalogo.classList.add('fondo-grisOscuro');
+    btnCatalogo.classList.toggle('fondo-negro');
+    btnCatalogo.classList.toggle('modoOscuro');
+    footer.classList.toggle('fondo-negro');
+    footer.classList.toggle('modoOscuro');
 });
